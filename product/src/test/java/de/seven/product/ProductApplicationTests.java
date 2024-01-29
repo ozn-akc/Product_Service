55a90b8390a9549e91d9cb1677ef4da95611d891
Seven <coding.s3v3n@gmail.com>
2024-01-29 10:14:22 +0100

    init

---
+++ product/src/test/java/de/seven/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package de.seven.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
